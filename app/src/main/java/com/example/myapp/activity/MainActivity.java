c5884c64d15541bdbe1ee3c608a59faba4e042c6
or-pilosof <or.pilosof@shutterfly.com>
2021-04-17 13:52:09 +0300

    Update MainActivity.java
    infinite scroll

--- app/src/main/java/com/example/myapp/activity/MainActivity.java
+++ app/src/main/java/com/example/myapp/activity/MainActivity.java
@@ -8,8 +8,8 @@ import com.example.myapp.model.PixabayImage;
 import com.example.myapp.model.PixabayResponse;
 import com.example.myapp.rest.PixabayService;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,8 +17,8 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,19 +29,24 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class     MainActivity extends AppCompatActivity {
     private static final String BASE_URL = "http://pixabay.com/";
     private static final String API_KEY = "12175339-7048b7105116d7fa1da74220c";
+    public static final int INIT_PAGE = 0;
+    public static final int PER_PAGE = 3;
 
     //const
 
     //fields
     private static Retrofit retrofit = null;
-    private RecyclerView recyclerView = null;
+    private RecyclerView recycler_view = null;
     private ArrayList<PixabayImage> cur_image_list = new ArrayList<>();
 
-    private EditText inputText;
-    private String query = "dogs";
+    private EditText input_text;
+    private String query = "";
+    private int cur_page = INIT_PAGE;
+    private int visibleImageCount, totalImageCount, lastImageCount=0;
+    private boolean recyclerViewLoading = true;
 
 
     @Override
@@ -49,21 +54,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // init fields
-        inputText = findViewById(R.id.inputQueryText);
-        inputText.setOnKeyListener(new View.OnKeyListener() {
+        input_text = findViewById(R.id.inputQueryText);
+        input_text.setOnKeyListener((v, keyCode, event) -> {
+            if (keyCode==KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                query= input_text.getText().toString();
+                this.cur_page=INIT_PAGE;
+                this.cur_image_list.clear();
+                connectAndGetApiData();
+                return true;
+            }
+            return false;
+        });
+
+        recycler_view = findViewById(R.id.recycler_view);
+        recycler_view.setHasFixedSize(true);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        recycler_view.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (keyCode==KeyEvent.KEYCODE_ENTER){
-                    query=inputText.getText().toString();
-                    connectAndGetApiData();
-                    return true;
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                visibleImageCount = recyclerView.getChildCount();
+                totalImageCount = recyclerView.getLayoutManager().getItemCount();
+                lastImageCount = ((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+                if (recyclerViewLoading){
+                    if (lastImageCount + visibleImageCount >= totalImageCount){
+                        connectAndGetApiData();
+                        recyclerViewLoading=false;
+                    }
                 }
-                return false;
+                else{
+                    recyclerViewLoading=true;
+                }
+
             }
         });
-        recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -88,23 +112,19 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // This method create an instance of Retrofit
-// set the base url
+
     public void connectAndGetApiData() {
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+            retrofit = getRetfofit();
         }
         PixabayService pixabayService = retrofit.create(PixabayService.class);
-        Call<PixabayResponse> call = pixabayService.getImages(API_KEY,query,"photo");
+        this.cur_page++;
+        Call<PixabayResponse> call = pixabayService.getImages(API_KEY,query,"photo",this.cur_page,PER_PAGE);
         call.enqueue(new Callback<PixabayResponse>() {
             @Override
             public void onResponse(Call<PixabayResponse> call, Response<PixabayResponse> response) {
                 List<PixabayImage> images = response.body().getHits();
-                saveData(images);
-                recyclerView.setAdapter(new PixabayImageAdapter(images,R.layout.image_item,getApplicationContext()));
+                addAndRenderNewData(images);
             }
 
             @Override
@@ -114,9 +134,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void saveData(List<PixabayImage> images) {
-        this.cur_image_list.clear();
+    private void renderImages(List<PixabayImage> images) {
+        recycler_view.setAdapter(new PixabayImageAdapter(images,R.layout.image_item,getApplicationContext()));
+    }
+
+    private Retrofit getRetfofit() {
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    private void addAndRenderNewData(List<PixabayImage> images){
         this.cur_image_list.addAll(images);
+        renderImages(this.cur_image_list);
     }
 
 }
