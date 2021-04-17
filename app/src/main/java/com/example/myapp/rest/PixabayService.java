12342a23d1671e59bef6e55775f6dfc5210ea446
or-pilosof <or.pilosof@shutterfly.com>
2021-04-17 13:51:58 +0300

    Update PixabayService.java
    added param to query

--- app/src/main/java/com/example/myapp/rest/PixabayService.java
+++ app/src/main/java/com/example/myapp/rest/PixabayService.java
@@ -9,5 +9,5 @@ import retrofit2.http.Query;
 
 public interface PixabayService {
     @GET("/api/")
-    Call<PixabayResponse> getImages(@Query("key") String apiKey, @Query("q") String query, @Query("image_type") String type);
+    Call<PixabayResponse> getImages(@Query("key") String api_key, @Query("q") String query, @Query("image_type") String type, @Query("page") int page, @Query("per_page") int per_page);
 }
